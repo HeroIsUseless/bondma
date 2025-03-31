6a40aa5241482c93433091df6cf40f4133c60ac6
heroisuseless <3256384696@qq.com>
2025-03-31 22:55:13 +0800

    fix

--- packages/server/src/service/project.service.ts
+++ packages/server/src/service/project.service.ts
@@ -34,7 +34,6 @@ export class ProjectService {
         name: 'Production',
         type: 'production',
         languages: data.languages ?? [],
-        tokens: [],
         projectId: project.id,
       },
     });
